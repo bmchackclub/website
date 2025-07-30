c463c0b9cad7db35b567653ac0a12220da2c8725
Crystal Bajgai <crystalbajgai@gmail.com>
2025-07-30 22:54:21 +0545

    Add WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="hero flex h-[90vh] flex-col items-center justify-center space-y-2">
+	<div class="text-primary">
+		<h1 class="text-4xl font-bold">BMC Hack Club</h1>
+	</div>
+	<div class="text-text">
+		<h1 class="text-2xl md:text-3xl">Website WIP 🏗️</h1>
+	</div>
+	<div class="text-text">
+		<h1 class="text-2xl md:text-3xl">
+			Join the club: <a href="https://bmchc.fillout.com/join" class="text-accent"> Click here! </a>
+		</h1>
+	</div>
+    <div class="text-text">
+		<h1 class="text-2xl md:text-3xl">
+			Discord: <a href="https://discord.gg/NjT949Cj" class="text-accent"> Click here! </a>
+		</h1>
+	</div>
+    <div class="text-text">
+		<h1 class="text-2xl md:text-3xl">
+			Instagram: <a href="https://www.instagram.com/bmchackclub" class="text-accent"> @bmchackclub </a>
+		</h1>
+	</div>
+</section>
